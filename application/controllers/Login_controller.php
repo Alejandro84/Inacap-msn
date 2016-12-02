d31a68d8a9a7dc0529c87b4bbfb8249ec215ad5e
Fabian Vasquez <vvfabiangonzalo@gmail.com>
2016-12-02 19:28:49 -0300

    hola

--- application/controllers/Login_controller.php
+++ application/controllers/Login_controller.php
@@ -2,16 +2,16 @@
 
 class Login_controller extends CI_Controller {
 
- function __construct()
- {
-   parent::__construct();
- }
+   function __construct()
+   {
+      parent::__construct();
+   }
 
- function index()
- {
-   $this->load->helper(array('form'));
-   $this->load->view('login');
- }
+   function index()
+   {
+      $this->load->helper('form');
+      $this->load->view('login');
+   }
 
 }
 
