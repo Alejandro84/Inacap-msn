2a4881641e1583277c8d73c89e15dc1abeb7d83d
Fabian Vasquez <vvfabiangonzalo@gmail.com>
2016-12-02 20:12:30 -0300

    listo?

--- application/views/mensaje/nuevo.php
+++ application/views/mensaje/nuevo.php
@@ -18,21 +18,20 @@
 
             <div class="form-group">
                <caption>Mensaje</caption>
-               <textarea rows="4" class="form-control" name="mensaje">
-               </textarea>
+               <textarea rows="4" class="form-control" name="mensaje"></textarea>
             </div>
 
             <button type="submit" name="guardar" class="btn btn-success">Enviar</button>
 
       </div>
       <div class="col-md-6">
-
-            <div class="form-group">
+         <h3>Seleccion los destinatarios</h3>
+            <div class="form-group well">
                <caption>Destinatarios</caption>
                <ul>
                   <li class="checkbox"><input type="checkbox" name="ing_informatica" value="">Ingenieria en informatica
                      <ul>
-                        <li class="checkbox"><input type="checkbox" name="semestre" value="">Semestre
+                        <li class="checkbox"><input type="checkbox" name="semestre" value="">Semestre II
                            <ul>
                               <li class="checkbox"><input type="checkbox" name="semestre" value="">Alejandro Vargas</li>
                               <li class="checkbox"><input type="checkbox" name="semestre" value="">Fabian Vasquez</li>
@@ -42,6 +41,24 @@
                      </ul>
                   </li>
                </ul>
+
+
+               <ul>
+                  <li class="checkbox"><input type="checkbox" name="ing_informatica" value="">Analista programador
+                     <ul>
+                        <li class="checkbox"><input type="checkbox" name="semestre" value="">Semestre I</li>
+                        <li class="checkbox"><input type="checkbox" name="semestre" value="">Semestre II</li>
+                        <li class="checkbox"><input type="checkbox" name="semestre" value="">Semestre III
+                           <ul>
+                              <li class="checkbox"><input type="checkbox" name="semestre" value="">Alejandro Vargas</li>
+                              <li class="checkbox"><input type="checkbox" name="semestre" value="">Fabian Vasquez</li>
+                              <li class="checkbox"><input type="checkbox" name="semestre" value="">Andres Otero</li>
+                           </ul>
+                        </li>
+                     </ul>
+                  </li>
+               </ul>
+
             </div>
 
       </div>
